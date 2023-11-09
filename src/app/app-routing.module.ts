bc23a11de67d5468c93c42a6469cc2056f141f26
AugustoEscalada <augustoescalada36@gmail.com>
2023-11-09 10:29:59 -0300

    estructura login y register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path:"login",
+loadChildren: ()=> import('./Pages/login/login.module').then(m => m.LoginModule)},
+
+{path:"register",
+loadChildren: ()=> import('./Pages/register/register.module').then(m => m.RegisterModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
